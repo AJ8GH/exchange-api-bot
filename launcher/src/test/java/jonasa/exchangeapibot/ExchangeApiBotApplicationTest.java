dd3f61406704b8faa4e50f1622661082dd782885
Adam Jonas <adam.jonas@ppb.com>
2022-02-20 23:01:11 +0000

    Create test properties

--- launcher/src/test/java/jonasa/exchangeapibot/ExchangeApiBotApplicationTest.java
+++ launcher/src/test/java/jonasa/exchangeapibot/ExchangeApiBotApplicationTest.java
@@ -2,8 +2,10 @@ package jonasa.exchangeapibot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ExchangeApiBotApplicationTest {
 
     @Test
